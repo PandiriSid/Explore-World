41e365e6f8a3f782f9fcd706a8612591da3d7ea7
PandiriSid <34942083+PandiriSid@users.noreply.github.com>
2024-08-20 11:31:35 -0700

    Add files via upload

---
+++ components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className="header-container">
+      <div className="header-content">
+        <h2 className="title">
+          <a href="/">Where in the world?</a>
+        </h2>
+        <p className="title">
+          <i className="fas fa-globe"></i>
+          &nbsp;&nbsp;Let's learn Geography
+        </p>
+      </div>
+    </header>
+  )
+}
